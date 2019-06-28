5d160af40d349957401684282dd80047085cd43a
Anand Tsogtbaatar <atsogt17@MacBook-Pro-2.local>
2019-06-27 20:34:15 -0400

    bugs-fixed

--- expert-challenge.js
+++ expert-challenge.js
@@ -13,12 +13,23 @@
     }
   };
 
-  Question.prototype.checkAnswer = function(ans) {
+  Question.prototype.checkAnswer = function(ans, callback) {
+    var sc;
+
     if (ans === this.correct) {
       console.log("Correct");
+      sc = callback(true);
     } else {
       console.log("Wrong, try again");
+
+      sc = callback(false);
     }
+    this.displayScore(sc);
+  };
+
+  Question.prototype.displayScore = function(score) {
+    console.log("Your current score is: " + score);
+    console.log("---------------------------------------");
   };
 
   var q1 = new Question("Is Javascript cool?", ["yes", "no", "maybe"], 0);
@@ -31,6 +42,18 @@
     1
   );
   var questions = [q1, q2, q3];
+
+  function score() {
+    var sc = 0;
+    return function(correct) {
+      if (correct) {
+        sc++;
+      }
+      return sc;
+    };
+  }
+  var keepScore = score();
+
   //instances
   function nextQuestion() {
     var n = Math.floor(Math.random() * questions.length);
@@ -39,8 +62,8 @@
 
     var answer = prompt("Please select the correct answer.");
 
-    if (answer !== "exit") {
-      questions[n].checkAnswer(parseInt(answer));
+    if (answer !== "quit") {
+      questions[n].checkAnswer(parseInt(answer), keepScore);
 
       nextQuestion();
     }
